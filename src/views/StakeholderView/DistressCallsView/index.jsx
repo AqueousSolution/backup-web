8040f5153a6ebc364e938ca78f37095217e755b3
Idahosa Akioya <Idahosa@initshq.com>
2021-09-12 15:43:56 +0100

    maps

--- src/views/StakeholderView/DistressCallsView/index.jsx
+++ src/views/StakeholderView/DistressCallsView/index.jsx
@@ -29,6 +29,9 @@ const DistressCallsView = () => {
         setLoading(false)
     },[stakeholderUser])
 
+    let key = process.env.REACT_APP_GOOGLE_MAP
+    let url = `https://maps.googleapis.com/maps/api/js?key=${key}&v=3.exp&libraries=geometry,drawing,places`
+
 
     return ( 
         <>
@@ -45,7 +48,7 @@ const DistressCallsView = () => {
                         <ContactList />
                         <ContactInfo />
                      { currentEmergency ?  <Map isMarkerShown={true}
-                        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBYq_xj2pTePbE7MmB4E4wxMB2W7kjBpac&v=3.exp&libraries=geometry,drawing,places"
+                        googleMapURL= {url}
                         loadingElement={<div style={{ height: `100%` }} />}
                         containerElement={<div style={{ height: `99%` }} />}
                         mapElement={<div style={{ height: `100%` }} />}/>
